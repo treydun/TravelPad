55afb160c5783439887d07ec0a788e9f5cf70a16
Treydun <treyhardson@gmail.com>
2019-01-19 18:54:23 -0500

    Cleaned and compressed, ready for prerelease

--- src/main/java/net/h31ix/travelpad/TravelPadBlockListener.java
+++ src/main/java/net/h31ix/travelpad/TravelPadBlockListener.java
@@ -1,6 +1,7 @@
 package net.h31ix.travelpad;
 
 import net.h31ix.travelpad.api.Pad;
+import net.h31ix.travelpad.api.UnnamedPad;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -20,7 +21,8 @@ import org.bukkit.inventory.EquipmentSlot;
 
 public class TravelPadBlockListener implements Listener {
 
-    private final String TRAVELPAD_SIGN_TAG = (ChatColor.GREEN+"[Travelpad]").intern();
+    private final String TRAVELPAD_SIGN_TAG = (ChatColor.GREEN + "[Travelpad]").intern();
+    private final String TRAVELPAD_SIGN_ERROR_TAG = (ChatColor.RED+ "[Travelpad]").intern();
 
     private Travelpad plugin;
 
@@ -43,51 +45,46 @@ public class TravelPadBlockListener implements Listener {
                             plugin.create(block.getLocation(), player);
                         }
                     } else {
-                        event.getPlayer().sendMessage(Travelpad.PLUGIN_PREFIX_COLOR + "There is already a Travelpad at this location!");
+                        plugin.errorMessage(event.getPlayer(), "There is already a Travelpad at this location!");
                     }
                 }
             } else if (block.getType() == Material.SIGN || block.getType() == Material.WALL_SIGN) {
-                {
-                    BlockState bState = block.getState();
-                    Sign sign = (Sign) bState;
-                    if(sign.getLine(0).equals(TRAVELPAD_SIGN_TAG)) {
-                        if (plugin.Manager().getPad(sign.getLine(1)) != null) {
-                            event.getPlayer().performCommand("t tp " + sign.getLine(1));
-                        } else {
-                            sign.setLine(0, ChatColor.RED + "[Travelpad]");
-                            sign.update(true);
-                            event.getPlayer().sendMessage(Travelpad.PLUGIN_PREFIX_COLOR + ChatColor.RED + "Error, unable to find the pad " + sign.getLine(1));
-                        }
-                    } else if (sign.getLine(0).toLowerCase().contains("travelpad")) {
-                        if (plugin.Manager().getPad(sign.getLine(1)) != null) {
-                            sign.setLine(0, TRAVELPAD_SIGN_TAG);
-                            sign.update(true);
-                            event.getPlayer().performCommand("t tp " + sign.getLine(1));
-                        } else {
-                            sign.setLine(0, ChatColor.RED + "[Travelpad]");
-                            sign.update(true);
-                            event.getPlayer().sendMessage(Travelpad.PLUGIN_PREFIX_COLOR + ChatColor.RED + "Error, unable to find the pad " + sign.getLine(1));
-                        }
+                BlockState bState = block.getState();
+                Sign sign = (Sign) bState;
+                if (sign.getLine(0).equals(TRAVELPAD_SIGN_TAG)) {
+                    if (plugin.Manager().getPad(sign.getLine(1)) != null) {
+                        event.getPlayer().performCommand("t tp " + sign.getLine(1));
+                    } else {
+                        sign.setLine(0, TRAVELPAD_SIGN_ERROR_TAG);
+                        sign.update(true);
+                        plugin.errorMessage(event.getPlayer(), "Error, unable to find the pad " + sign.getLine(1));
                     }
-                    //Legacy sign support
-                    if (sign.getLine(1).startsWith("/t tp ")) {
-                        event.getPlayer().performCommand(sign.getLine(1).substring(1));
+                } else if (sign.getLine(0).toLowerCase().contains("travelpad")) {
+                    if (plugin.Manager().getPad(sign.getLine(1)) != null) {
+                        sign.setLine(0, TRAVELPAD_SIGN_TAG);
+                        sign.update(true);
+                        event.getPlayer().performCommand("t tp " + sign.getLine(1));
+                    } else {
+                        sign.setLine(0, TRAVELPAD_SIGN_ERROR_TAG);
+                        sign.update(true);
+                        plugin.errorMessage(event.getPlayer(), "Error, unable to find the pad " + sign.getLine(1));
                     }
+                }
+                //Legacy sign support
+                if (sign.getLine(1).startsWith("/t tp ")) {
+                    event.getPlayer().performCommand(sign.getLine(1).substring(1));
                 }
             }
         }
-
     }
 
     @EventHandler(priority = EventPriority.LOW)
     public void onBlockPlace(BlockPlaceEvent event) {
-        //TODO: Optimize... (Switched to single lookup for efficiency. Needs more thought)
         Block block = event.getBlock();
         Location location = block.getLocation();
-        //Was getPadNear()
         if (plugin.Manager().getPadAt(location) != null || plugin.Manager().getUnnamedPadAt(location) != null) {
             event.setCancelled(true);
-            event.getPlayer().sendMessage(Travelpad.PLUGIN_PREFIX_COLOR + ChatColor.RED + "Why are you placing a block where a tpad is, this is a bug!");
+            plugin.errorMessage(event.getPlayer(), "Why are you placing a block where a tpad is, this is a bug!");
         } else {
             if (block.getType() == plugin.Config().center) {
                 if (block.getRelative(BlockFace.EAST).getType() == plugin.Config().outline
@@ -105,27 +102,31 @@ public class TravelPadBlockListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onBlockBreak(BlockBreakEvent event) {
-        //TODO: Optimize... (Switched to single lookup for efficiency. Needs more thought)
         Block block = event.getBlock();
         if (block.getType() == plugin.Config().center) {
-            //Was getPadNear()
             Pad pad = plugin.Manager().getPadAt(block.getLocation());
             if (pad != null) {
                 if (event.getPlayer().hasPermission("travelpad.delete")) {
-                    //TODO: should be travelpad.admin?
-                    // travelpad.delete.other?
                     if (plugin.Config().anyBreak || plugin.Manager().isOwner(event.getPlayer(), pad) || event.getPlayer().hasPermission("travelpad.delete.any")) {
                         plugin.message(event.getPlayer(), plugin.Lang().delete_approve());
                         plugin.delete(pad);
                     } else {
-                        event.getPlayer().sendMessage(ChatColor.RED + plugin.Lang().command_deny_permission());
+                        plugin.errorMessage(event.getPlayer(), plugin.Lang().command_deny_permission());
                         event.setCancelled(true);
                     }
                 } else {
-                    event.getPlayer().sendMessage(ChatColor.RED + plugin.Lang().command_deny_permission());
+                    plugin.errorMessage(event.getPlayer(), plugin.Lang().command_deny_permission());
+                    event.setCancelled(true);
                 }
             } else if (plugin.Manager().getUnnamedPadAt(block.getLocation()) != null) {
-                event.setCancelled(true);
+                UnnamedPad uPad = plugin.Manager().getUnnamedPadAt(block.getLocation());
+                if (uPad.OwnerUUID().equals(event.getPlayer().getUniqueId())) {
+                    plugin.message(event.getPlayer(), plugin.Lang().delete_approve());
+                    plugin.Manager().removePad(uPad);
+                } else {
+                    plugin.errorMessage(event.getPlayer(), plugin.Lang().command_deny_permission());
+                    event.setCancelled(true);
+                }
             }
         }
         /* remove the 'any' block break check. resource heavy with lots of pads
